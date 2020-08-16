36d09a45c2acfa9859511824eddc590e86930fc5
Wei <a0983161756@gmail.com>
2020-08-17 00:55:30 +0800

    modifty v.02

--- AlamofireTest/HttpBike.swift
+++ AlamofireTest/HttpBike.swift
@@ -15,6 +15,7 @@ class HttpBike {
 
     func request<T>(url: String, method: HTTPMethod, parameters: Parameters?, type: T.Type, success: ((Decodable) -> Void)?, failure:(() -> Void)?) where T: Decodable {
         session.request(url, method: method, parameters: parameters, encoding: URLEncoding.queryString, headers: nil).response { (response) in
+            //200成功抓取到
             if response.response!.statusCode == 200 {
                 do {
                     let result = try JSONDecoder().decode(type, from: response.data!)
